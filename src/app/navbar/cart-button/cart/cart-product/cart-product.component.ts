7a7079cae8b9b9ce5f44197265c506e727f1ae62
lorenpap <57562403+lorenpap@users.noreply.github.com>
2020-05-19 16:54:25 +0300

    cr changes - renaming

--- src/app/navbar/cart-button/cart/cart-product/cart-product.component.ts
+++ src/app/navbar/cart-button/cart/cart-product/cart-product.component.ts
@@ -9,30 +9,29 @@ import {Product} from '../../../../interfaces/product-interface';
 })
 export class CartProductComponent implements OnInit {
   @Input() product: CartItem;
-  @Output() changeLimitedAmount = new EventEmitter<number>();
-  @Output() productToRemove = new EventEmitter<Product>();
-  @Output() changeUnlimitedAmount = new EventEmitter<number>();
-  value: number;
-  limit: number[];
+  @Output() changeAmount: EventEmitter<number>;
+  @Output() productToRemove: EventEmitter<Product>;
+  limitOptions: number[];
 
   constructor() {
+    this.changeAmount = new EventEmitter<number>();
+    this.productToRemove = new EventEmitter<Product>();
   }
 
   ngOnInit() {
-    this.limit = [...Array(this.product.product.limit).keys()].map(x => x + 1);
-    this.value = this.product.amount;
+    this.limitOptions = [...Array(this.product.product.limit).keys()].map(x => x + 1);
   }
 
   removeProduct() {
     this.productToRemove.emit(this.product.product);
   }
 
-  getValue(value: number) {
-    this.changeLimitedAmount.emit(value);
+  changeLimit(limit: number) {
+    this.changeAmount.emit(limit);
   }
 
-  getInputAmount() {
+  unlimitedProductAmount() {
     console.log(this.product.amount);
-    this.changeUnlimitedAmount.emit(this.product.amount);
+    this.changeAmount.emit(this.product.amount);
   }
 }
